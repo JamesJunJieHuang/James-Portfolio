e8b157ba24a2dcaf9dd57a475cea769fa153bda2
JamesJunJieHuang <108486354+JamesJunJieHuang@users.noreply.github.com>
2023-04-25 17:38:30 -0400

    dog route

--- app/api/dog/route.ts
+++ app/api/dog/route.ts
@@ -1,11 +1,20 @@
-// export async function GET(request: Request) {
-//   return new Response("Hello, DOG!");
+export async function GET(request: Request) {
+    console.log('get request received')
+  return new Response("Hello, DOG!");
+}
+
+export async function POST(request: Request) {
+  console.log("post request received");
+  return new Response("post, DOG!");
+}
+
+// function getDog(req: Request, res: Response) {
+//   if (req.method === "POST") {
+//     console.log("POST REQUEST RECEIVED");
+//   } else if (req.method === "GET") {
+//     console.log("POST REQUEST RECEIVED");
+//   }
+//   return;
 // }
 
-export function getDog(req: Request, res: Response) {
-  if (req.method === "POST") {
-    console.log("POST REQUEST RECEIVED");
-  } else if (req.method === "GET") {
-    console.log("POST REQUEST RECEIVED");
-  }
-}
+// export default getDog;
